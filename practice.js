4a72e1033be2f961095d8f567327919b23711431
kspayne93 <kyle@investavante.com>
2018-11-27 15:03:04 -0700

    solved problems 1-4

--- practice.js
+++ practice.js
@@ -12,12 +12,16 @@
 var arr = [10,20,30];
 // Do not edit the code above.
 
+
 /*
   Create a function named 'first' that is given 'arr' as an argument.
   Return the first item in the given array.
 */
 
 //Code Here
+function first (arr) {
+  return arr[0]
+}
 
 
 
@@ -33,6 +37,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
+function last (arr) {
+  return arr.pop()
+}
 
 
 
@@ -48,7 +55,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper (family) {
+for (let i = 0; i < family.length; i++) {
+  alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +74,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper (letters) {
+  for (let i = letters.length -1; i>=0; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
